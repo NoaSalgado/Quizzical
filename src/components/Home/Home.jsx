80becf82d561fd3a1ac4165c7f7a086e539e393a
Noa <noasalgadodev@gmail.com>
2023-05-21 12:08:42 +0200

    Feat: add home and button components

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Button from '../Button/Button';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <header className='header'>
+      <h1 className='main-heading'>Quizzical</h1>
+      <Button>Start Quizz</Button>
+    </header>
+  );
+};
+
+export default Home;
