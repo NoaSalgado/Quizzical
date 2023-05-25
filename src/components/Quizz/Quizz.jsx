bc5f4be544f5f3db2ca238b5d83af29fa86fb165
Noa <noasalgadodev@gmail.com>
2023-05-25 12:08:36 +0200

    Feat: add loader

--- src/components/Quizz/Quizz.jsx
+++ src/components/Quizz/Quizz.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 
+import Loader from '../Loader/Loader';
 import Question from '../Question/Question';
 import Button from '../Button/Button';
 import './Quizz.css';
 
 const Quizz = ({ quizzStatus, endQuizz, startGame }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const [questions, setQuestions] = useState();
   const [correctAnswrs, setCorrectAnswers] = useState(0);
 
@@ -13,38 +15,47 @@ const Quizz = ({ quizzStatus, endQuizz, startGame }) => {
   function getQuestions() {
     fetch(`https://opentdb.com/api.php?amount=5`)
       .then((response) => response.json())
-      .then((data) => setQuestions(data.results));
+      .then((data) => {
+        setQuestions(data.results);
+        setIsLoading(false);
+      });
   }
 
   function restartQuizz() {
     startGame();
+    setIsLoading(true);
     getQuestions();
     setCorrectAnswers(0);
   }
 
   return (
-    <div className='quizz'>
-      {questions &&
-        questions.map((question, index) => (
-          <Question
-            key={index}
-            question={question}
-            quizzStatus={quizzStatus}
-            setCorrectAnswers={setCorrectAnswers}
-          />
-        ))}
-      {quizzStatus === 'started' && (
-        <Button handleClick={endQuizz}>Check Answers</Button>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className='quizz'>
+          {questions.map((question, index) => (
+            <Question
+              key={index}
+              question={question}
+              quizzStatus={quizzStatus}
+              setCorrectAnswers={setCorrectAnswers}
+            />
+          ))}
+          {quizzStatus === 'started' && (
+            <Button handleClick={endQuizz}>Check Answers</Button>
+          )}
+          {quizzStatus === 'finished' && (
+            <>
+              <p>
+                You scored {correctAnswrs}/{questions.length} correct answers
+              </p>
+              <Button handleClick={restartQuizz}>Play again</Button>
+            </>
+          )}
+        </div>
       )}
-      {quizzStatus === 'finished' && (
-        <>
-          <p>
-            You scored {correctAnswrs}/{questions.length} correct answers
-          </p>
-          <Button handleClick={restartQuizz}>Play again</Button>
-        </>
-      )}
-    </div>
+    </>
   );
 };
 
